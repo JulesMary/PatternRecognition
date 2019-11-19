0152cfcd0485fcd6cb9515f3e72c1308775f1ac6
J. Martensen <jule.m.martensen@student.fh-kiel.de>
2019-11-19 19:06:11 +0100

    Update lab1.m

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -6,10 +6,10 @@ hold off;
 N = 100;
 
 
-%fix rounds towards zero
+%fix rounds towards zero, so the values for A are in the interval [0, 10)
 %rand creates uniformly distributed random numbers
 A = fix(10*rand(N));
-% round rounds to the nearest integer
+% round rounds to the nearest integer so the values for B are in the interval [0, 10]
 % randn creates normally distributed numbers with zero mean and variance one.
 B = round(10*randn(N));
 
@@ -30,26 +30,28 @@ for i=minB:maxB
   b = [b length(find (B==i))];
 end
 
+%figure showing the frequency distribution for a
 figure(1);
 bar(minA:maxA, a);
-
 axis([minA-1 maxA+1]);
 title("Frequency Distribution A")
 xlabel("Value")
 ylabel("Frequency")
 
+%line indicating the mean
 hold on;
 line([minA-1 maxA+1],[mean(a(:)) mean(a(:))], 'Color','r');
-
 hold off;
+
+%figure showing the frequency distribution for b
 figure(2);
 bar(minB:maxB, b);
-
 axis([minB-1 maxB+1]);
 title("Frequency Distribution B")
 xlabel("Value")
 ylabel("Frequency")
 
+%compute the relative frequencies
 relFr_a = a./N^2;
 relFr_b = b./N^2;
 
@@ -67,6 +69,7 @@ title("Relative Frequencies B")
 xlabel("Value")
 ylabel("Rel Frequency")
 
+% compute the probability masses -> should sum up to 1
 sumA = sum(relFr_a)
 sumB = sum(relFr_b)
 
@@ -74,17 +77,18 @@ sumB = sum(relFr_b)
 x = minB:0.1:maxB;
 m = mean(B(:));
 s = std(B(:));
+% 
 f = exp(-0.5*((x-m)./s).^2)./(sqrt(2*pi)*s);
 hold on;
 plot(x, f, 'g');
 
+%check intervals
 inInterval = length(find(B > (s*-1) & B<s));
 lengthB = length(B(:));
 bInS = inInterval/lengthB
 
 inInterval2 = length(find(B > (s*-2) & B<2*s));
 bInS2 = inInterval2/lengthB
-
 
 inInterval3 = length(find(B > (s*-3) & B<3*s));
 bInS3 = inInterval3/lengthB
